20b64af7c579f1af8cb5af6539ef8956b62949dd
comejv <comevincent03@gmail.com>
2021-09-07 18:44:22 +0200

    rangement code et ajout fonctions optimisation

--- main.py
+++ main.py
@@ -3,20 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Paramètres
-V0 = float(
-    input("Quelle est la vitesse initiale de lancer en mètres par seconde ?"))
-T = float(input("Quelle est la hauteur à laquelle le ballon est lancé ?"))
-
-r = 0.24  # Diamètre ballon
-g = 9.8  # Accélération gravitationelle en m.s-2
-pos_planche = 6.75
-D_panier = 0.45
-support_panier = 0.15
-
-x_panier = [6.15, 6.6]
-h_panier = 3.05 - T  # Hauteur panier relativement au départ du ballon
-y_panier = [h_panier, h_panier]
+# Fonctions
 
 
 def ACosTan(A):
@@ -31,9 +18,6 @@ def ACosTan(A):
     return Acos, Atan
 
 
-x = np.linspace(0, 10, 30)
-
-
 def position(acceleration_gravitationelle, vitesse_initiale, cosinus_angle, tangeante_angle):
     hauteur = -0.5 * acceleration_gravitationelle * (
         (x**2) / ((vitesse_initiale**2) * (cosinus_angle**2))
@@ -42,13 +26,41 @@ def position(acceleration_gravitationelle, vitesse_initiale, cosinus_angle, tang
     return hauteur
 
 
+def del_diff_pts(liste1, liste2):
+    diff = len(liste1)-len(liste2)
+    if diff < 0:
+        abs(diff)
+        del(liste2[diff:len(liste2)])
+    # FINIR CAS INVERSE
+
+        
+# Paramètres
+V0 = float(
+    input("Quelle est la vitesse initiale de lancer en mètres par seconde ?"))
+T = float(input("Quelle est la hauteur à laquelle le ballon est lancé ?"))
 Acos, Atan = ACosTan(float(input("Quel est l'angle de lancer en degrés ?")))
 
-centre_inertie = position(g, V0, Acos, Atan)
+# Constantes
+r = 0.24  # Diamètre ballon
+pos_planche = 6.75  # Distance origine repère - fond terrain
+D_panier = 0.45  # Diamètre du panier
+x_arceau = [6.15, 6.6]
+h_arceau = 3.05 - T  # Hauteur panier relativement au départ du ballon
+y_arceau = [h_arceau, h_arceau]
+g = 9.8  # Accélération gravitationelle en m.s-2
 
-# plt.plot(x, centre_inertie, 'r:')
+# Positions ballon
+x = np.linspace(0, 10, 60)
+centre_inertie = position(g, V0, Acos, Atan)
+# Enlever les valeurs négatives
+centre_inertie = [item for item in centre_inertie if item >= 0]
+
+# if len(centre_inertie) != len(x):
+    # FINIR ICI
+
+# Affichage courbes
 plt.plot(x, centre_inertie, color='red', linewidth=4, linestyle=':')
-plt.plot(x_panier, y_panier, marker="x", color="blue", linestyle=":")
+plt.plot(x_arceau, y_arceau, marker="x", color="blue", linestyle=":")
 plt.ylim(0, 5)
 plt.xlim(0, 10)
 
