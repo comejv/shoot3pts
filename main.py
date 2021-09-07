9ec9885c020ac4633c85f72127f52e5e51bce0ee
comejv <comevincent03@gmail.com>
2021-09-07 10:40:25 +0200

    suppr conversion en deg du cos

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ V0 = 10
 g = 9.8
 A = 45
 
-A = np.radians(A)  # Problème : l'équation est en degrés !
+Arad = np.radians(A)  # Problème : l'équation est en degrés !
 
 print(A) # La conversion en radians semble être OK
 
-x = np.linspace(0, 15, 60)
+Acos = np.cos(Arad)
 
-#ERREUR AU NIVEAU DU CALCUL DE COS
+Atan = np.tan(Arad)
+
+x = np.linspace(0, 15, 30)
+
 y = -0.5 * g * (
-    (x**2) / ((V0**2) * (np.degrees(np.cos(A))**2))
-    ) + np.degrees(np.tan(A)) * x
+    (x**2) / ((V0**2) * (Acos**2))
+    ) + Atan * x
 
 plt.plot(x, y) 
 
