d348a4afc309db440700733ddce006fb7553ec0a
comejv <comevincent03@gmail.com>
2021-09-08 12:25:43 +0200

    clean, optimisation, début volume ballon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# Le but est de trouver les vecteurs V et A pour que le ballon lancé rentre dans le panier sans toucher l'arceau
-
+# Author : Côme VINCENT
+# Copyrights 2021
+# Python 3.6
+# ? Le but est de trouver les vecteurs V et A pour que le ballon lancé rentre dans le panier sans toucher l'arceau
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -20,20 +22,12 @@ def ACosTan(A):
 
 def position(acceleration_gravitationelle, vitesse_initiale, cosinus_angle, tangeante_angle):
     hauteur = -0.5 * acceleration_gravitationelle * (
-        (x**2) / ((vitesse_initiale**2) * (cosinus_angle**2))
-    ) + tangeante_angle * x
+        (temp_x**2) / ((vitesse_initiale**2) * (cosinus_angle**2))
+    ) + tangeante_angle * temp_x
 
     return hauteur
 
 
-def del_diff_pts(liste1, liste2):
-    diff = len(liste1)-len(liste2)
-    if diff < 0:
-        abs(diff)
-        del(liste2[diff:len(liste2)])
-    # FINIR CAS INVERSE
-
-        
 # Paramètres
 V0 = float(
     input("Quelle est la vitesse initiale de lancer en mètres par seconde ?"))
@@ -49,17 +43,25 @@ h_arceau = 3.05 - T  # Hauteur panier relativement au départ du ballon
 y_arceau = [h_arceau, h_arceau]
 g = 9.8  # Accélération gravitationelle en m.s-2
 
-# Positions ballon
-x = np.linspace(0, 10, 60)
-centre_inertie = position(g, V0, Acos, Atan)
+# Centre inertie ballon
+temp_x = np.linspace(0, 10, 60)
+y = position(g, V0, Acos, Atan)
 # Enlever les valeurs négatives
-centre_inertie = [item for item in centre_inertie if item >= 0]
+y = [item for item in y if item >= 0]
+# Création array à la bonne taille p/r à y
+x = np.linspace(0, temp_x[len(y)], len(y))
 
-# if len(centre_inertie) != len(x):
-    # FINIR ICI
+# ! L'équation ne marche, pas, elle retourne les mêmes valeurs que y
+# Bas ballon
+y2 = position(g, V0, Acos, Atan)
+# Enlever les valeurs négatives
+y2 = [item for item in y if item >= 0]
+# Création array à la bonne taille p/r à y
+x2 = np.linspace(0, temp_x[len(y2)], len(y2))
 
 # Affichage courbes
-plt.plot(x, centre_inertie, color='red', linewidth=4, linestyle=':')
+plt.plot(x, y, color='red', linewidth=4, linestyle=':')
+plt.plot(x2, y2, color='blue', linewidth=1, linestyle='-')
 plt.plot(x_arceau, y_arceau, marker="x", color="blue", linestyle=":")
 plt.ylim(0, 5)
 plt.xlim(0, 10)
